61e0c54894f590f7dd5747609361a737f419b69b
Unhenryable <155365644+Unhenryable01@users.noreply.github.com>
2024-11-13 19:04:41 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { ArrowLeft, ExternalLink, Popcorn, X } from 'lucide-react'
 import Link from "next/link"
 import { useEffect, useState } from "react"
 
-import { Alert, AlertDescription } from "/components/ui/alert"
+import { Alert, AlertDescription } from "@/components/ui/alert"
 import { Button } from "@/components/ui/button"
 import {
   Dialog,
